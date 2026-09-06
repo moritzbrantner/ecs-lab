ee772a9fc06fb640a2cb7f9c8e85a268c14a67a5
moritzbrantner <31051084+moritzbrantner@users.noreply.github.com>
2026-09-06 08:05:44 +0200

    Validate temporal physics module in Pages build

--- scripts/build-pages.sh
+++ scripts/build-pages.sh
@@ -8,6 +8,7 @@ cargo build --locked --release -p ecs-web-demo --target wasm32-unknown-unknown
 node --input-type=module --check < site/app.js
 node --input-type=module --check < site/webgpu.js
 node --input-type=module --check < site/physics/app.js
+node --input-type=module --check < site/physics/temporal.js
 
 rm -rf pages-dist
 mkdir -p pages-dist/pkg
