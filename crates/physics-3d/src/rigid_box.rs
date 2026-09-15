815767653e693801b523800c2d4d5ac0cbc23310
moritzbrantner <31051084+moritzbrantner@users.noreply.github.com>
2026-09-15 02:02:33 +0200

    Apply current rustfmt to rigid-box state

--- crates/physics-3d/src/rigid_box.rs
+++ crates/physics-3d/src/rigid_box.rs
@@ -12,11 +12,7 @@ pub struct RigidBoxState3d {
 
 impl RigidBoxState3d {
     #[must_use]
-    pub const fn new(
-        center: Position,
-        linear_velocity: Velocity,
-        angular: AngularState3d,
-    ) -> Self {
+    pub const fn new(center: Position, linear_velocity: Velocity, angular: AngularState3d) -> Self {
         Self {
             center,
             linear_velocity,
